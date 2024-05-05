84222786889b046f8e50cb81ac2e1359d4be07b9
David Pérez <98660771+MichiMoments@users.noreply.github.com>
2024-05-05 16:10:14 -0500

    PASO12MELO

--- Caso3/src/Cliente.java
+++ Caso3/src/Cliente.java
@@ -7,15 +7,17 @@ import java.net.Socket;
 import java.nio.charset.StandardCharsets;
 import java.security.PublicKey;
 import java.security.SecureRandom;
+import java.util.Arrays;
 import java.util.Map;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 
 
 public class Cliente {
     public static final int PUERTO = 3400;
 	public static final String SERVIDOR = "localhost";
-    private PublicKey publicKey = null;
-    private String numeroAleatorio = null;
     private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
 	
 	public static void main(String[] args) throws Exception {
@@ -24,7 +26,7 @@ public class Cliente {
 		ObjectOutputStream escritor = null;
 		ObjectInputStream lector = null;
 		
-		System.out.println("Comienza cliente");
+		System.out.println("Comienza cliente\n");
 		
 		try {
 			socket = new Socket(SERVIDOR, PUERTO);
@@ -48,15 +50,33 @@ public class Cliente {
         //Paso 4 y 5
         String retoDevuleto = paso4(lector, escritor);
         if(retoDevuleto.equals(numeroAleatorio)){
-            System.out.println("Enviado mensaje OK");
+            System.out.println("Enviado mensaje OK (Paso 4-5)\n");
             escritor.writeObject("OK");
         } else {
-            System.out.println("Enviado mensaje ERROR");
+            System.out.println("Enviado mensaje ERROR (Paso 4-5)\n");
             escritor.writeObject("ERROR");
         }
 
-        //Paso 8
-        paso8(lector, escritor);
+        //Paso 8, 9 y 10 
+        BigInteger numMagic = paso8_9_10(lector, escritor);
+
+        //Paso 11 en el main
+        byte[] llaveByte = CifrarDecifrar.stringToHash(numMagic.toString());
+        int mitad = llaveByte.length / 2;
+
+        //preK_AB1
+        byte[] prek_ab1 = Arrays.copyOfRange(llaveByte, 0, mitad);
+
+        //preK_AB2
+        byte[] prek_ab2 = Arrays.copyOfRange(llaveByte, mitad, llaveByte.length);
+
+        //Llaves simetricas
+        SecretKey kab1 = new SecretKeySpec(prek_ab1, "AES");
+        SecretKey kab2 = new SecretKeySpec(prek_ab2, "HmacSHA256");
+
+        String cont = (String) lector.readObject();
+        System.out.println("Cliente recibe mensaje " + cont + "\n");
+
 
         escritor.close();
 		lector.close();
@@ -64,14 +84,14 @@ public class Cliente {
 		stdIn.close();
     }
 
-    public static void paso1(ObjectInputStream pIn, ObjectOutputStream pOut,String numeroAleatorio) throws Exception {
+    private static void paso1(ObjectInputStream pIn, ObjectOutputStream pOut,String numeroAleatorio) throws Exception {
         long tiempoInicial = System.currentTimeMillis();
         pOut.writeObject(numeroAleatorio);
-        System.out.println("Reto: " + numeroAleatorio);
+        System.out.println("Reto enviado: " + numeroAleatorio + "\n");
         
     }
 
-    public static String paso4(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
+    private static String paso4(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
         String retoCifrado = (String) pIn.readObject();
         PublicKey publicKey = (PublicKey) pIn.readObject();
         
@@ -79,38 +99,47 @@ public class Cliente {
         byte[] byteDescifrado = CifrarDecifrar.DescifrarAES(publicKey, byteCifrado);
 
         String retoDecifrado = new String(byteDescifrado);
-        System.out.println("Reto decifrado: " + retoDecifrado);
+        System.out.println("Reto recibido y decifrado: " + retoDecifrado + "\n");
 
         return retoDecifrado;
     }
 
-    public static void paso8(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
+    private static BigInteger paso8_9_10(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
         String g = (String) pIn.readObject();
         String p = (String) pIn.readObject();
         BigInteger gx = (BigInteger) pIn.readObject();
         String iv = (String) pIn.readObject();
-
-        String hexPGX = (String) pIn.readObject();
-        System.out.println("hexPGX: " + hexPGX);
-        //hasta aqui son iguales
+        byte[] bytePGX = (byte[]) pIn.readObject();
         PublicKey publicKey = (PublicKey) pIn.readObject();
 
         //Decfirar pgx
-        byte[] bytePGX = CifrarDecifrar.hexToBytes(hexPGX);
-        System.out.println("bytePGX: " + bytePGX);
         byte[] byteDescifradoPGX = CifrarDecifrar.DescifrarAES(publicKey, bytePGX);
-        System.out.println("byteDescifradoPGX: " + byteDescifradoPGX);
-        String strPGX = new String(byteDescifradoPGX);
-        System.out.println("strPGX: " + strPGX);
+
+        //Crear hash local
+        String pgx = g + ";" + p + ";" + gx.toString();
+        byte[] pgxhash = CifrarDecifrar.stringToHash(pgx);
         
+        if(Arrays.toString(byteDescifradoPGX).equals(Arrays.toString(pgxhash))){
+            System.out.println("Enviado mensaje OK (Paso 8-9)\n");
+            pOut.writeObject(true);;
+        } else {
+            System.out.println("Enviado mensaje ERROR (Paso 8-9)\n");
+            pOut.writeObject(false);;
+        }
 
-        System.out.println(g);
-        System.out.println(p);
-        System.out.println(gx);
-        System.out.println(iv);
-        // System.out.println(new String(byteDescifradoPG));
-        // System.out.println(new String(byteDescifradoGX));
+        //Paso 10
+        BigInteger y = BigInteger.valueOf(new java.util.Random().nextInt(20));
+        BigInteger base = new BigInteger(g);
+        BigInteger exponent = y;
+        BigInteger modulus = new BigInteger(p, 16);
+        BigInteger gy = base.modPow(exponent, modulus);
 
+        //Enviamos Gy
+        pOut.writeObject(gy);
+
+        //Generar Gx^y
+        BigInteger gxy = gx.modPow(y, modulus);
+        return gxy;
     }
 
 }
--- Caso3/src/ServidorDelegado.java
+++ Caso3/src/ServidorDelegado.java
@@ -6,7 +6,11 @@ import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.PrivateKey;
 import java.security.PublicKey;
+import java.util.Arrays;
 import java.util.Random;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class ServidorDelegado extends Thread{
     private Socket socket = null;
@@ -20,7 +24,7 @@ public class ServidorDelegado extends Thread{
     }
 
     public void run(){
-        System.out.println("Servidor delegado creado");
+        System.out.println("\nServidor delegado creado\n");
 
         try{
             ObjectInputStream escritor = new ObjectInputStream(socket.getInputStream());
@@ -35,8 +39,25 @@ public class ServidorDelegado extends Thread{
             //Pre paso 6
             prePaso6(escritor, lector);
 
-            //Paso 6 y 7
-            paso6(escritor, lector);
+            //Paso 6, 7, 9 y 10
+            BigInteger numMagic = paso6_9_10(escritor, lector);
+
+            //Paso 11 Dentro del main
+            byte[] llaveByte = CifrarDecifrar.stringToHash(numMagic.toString());
+            int mitad = llaveByte.length / 2;
+
+            //preK_AB1
+            byte[] prek_ab1 = Arrays.copyOfRange(llaveByte, 0, mitad);
+
+            //preK_AB2
+            byte[] prek_ab2 = Arrays.copyOfRange(llaveByte, mitad, llaveByte.length);
+
+            //Llaves simetricas
+            SecretKey kab1 = new SecretKeySpec(prek_ab1, "AES");
+            SecretKey kab2 = new SecretKeySpec(prek_ab2, "HmacSHA256");
+            
+            lector.writeObject("CONTINUAR");
+            System.out.println("Servidor escribe mensaje CONTINUAR\n");
 
         } catch (Exception e){
             e.printStackTrace();
@@ -46,7 +67,7 @@ public class ServidorDelegado extends Thread{
 
     private byte[] paso2(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
         String reto = (String) pIn.readObject();
-        System.out.println("Texto cifrado: " + reto);
+        System.out.println("Texto cifrado: " + reto + "\n");
         
         //Generar hash y cifrado
         byte[] hash = hash(reto);
@@ -69,10 +90,10 @@ public class ServidorDelegado extends Thread{
 
     private static void prePaso6(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
         String respuesta = (String) pIn.readObject();
-        System.out.println("Recibe mensaje " + respuesta);
+        System.out.println("Recibe mensaje " + respuesta + " (Paso 5)\n");
     }
 
-    private void paso6(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
+    private BigInteger paso6_9_10(ObjectInputStream pIn, ObjectOutputStream pOut) throws Exception {
         String g = "2";
         String p = "00dd8922190d814d016f28ad5b7b965b4b8cd7ac3defbe234fb57ed951c30596f648824bef16b4faa3700a5e2304ec0101c72da493d3d6cecfbfdf5b4634bf8fc92a769d7716ca82afdab111da1750c67f256329f69738961804a3f7c68c32716174482dc9bdff08d17ee4530ac3e3f0298104d99799a37acf742a23e1c6611d0f";
         String iv = "9372546801234875";
@@ -93,21 +114,26 @@ public class ServidorDelegado extends Thread{
 
         //Enviamos g, p y gx cifrados
         String pgx = g + ";" + p + ";" + gx.toString();
-
         byte[] pgxhash = CifrarDecifrar.stringToHash(pgx);
-        System.out.println("hash pgx" + pgxhash);
         byte[] pgxcipher = CifrarDecifrar.CifrarAESConHash(privateKey, pgxhash);
-        
-        System.out.println("cifrado pgx" + pgxcipher);
-
-        //Convertirlos en hexa el cifrado
-        String pgxcipherhex = CifrarDecifrar.bytesToHex(pgxcipher);
-        System.out.println("cifrado pgx hex: " + pgxcipherhex);
-        pOut.writeObject(pgxcipherhex);
-        
+        pOut.writeObject(pgxcipher);
 
         //Enviar llave publica
         pOut.writeObject(publicKey);
+        
+        //Recibir respuesta
+        boolean respuesta = (boolean) pIn.readObject();
+        if(respuesta){
+            System.out.println("Recibe mensaje OK (Paso 9)\n");
+        } else {
+            System.out.println("Recibe mensaje ERROR (Paso 9)\n");
+        }
+
+        //Gemerar Gy ^x
+        BigInteger gy = (BigInteger) pIn.readObject();
+        BigInteger gyx = gy.modPow(x, modulus);
+
+        return gyx;
     }
 
     private static byte[] hash(String mensaje) throws Exception {
